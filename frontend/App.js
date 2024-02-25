7ec54eb8436b3afb0e84b3d9bfacc5df1e3158fd
ishandeshpande <63430023+ishandeshpande@users.noreply.github.com>
2024-02-25 01:37:18 -0600

    Revert "got the pdf upload working"
    This reverts commit 193d46cdc02e522bdc3ae25ddbb32d4e5cf19201.

--- frontend/App.js
+++ frontend/App.js
@@ -48,20 +48,24 @@ const SignInScreen = ({ navigation }) => {
 // UploadResumeScreen Component
 const UploadResumeScreen = ({ navigation }) => {
   const handleUploadPress = async () => {
-    const result = await DocumentPicker.getDocumentAsync({
-      type: "application/pdf",
-      multiple: false,
-      copyToCacheDirectory: true
-    });
-    if (!result.cancelled) {
-      Alert.alert('File Uploaded', result.assets[0].name);
-      navigation.navigate("JobInterests")
-    } else {
-      setTimeout(() => {
-        Alert.alert('Document picked', JSON.stringify(result, null, 2));
-      }, 100);
+    try {
+      let result = await DocumentPicker.getDocumentAsync({
+        type: "application/pdf",
+        copyToCacheDirectory: true,
+        multiple: false,
+      });
+
+      if ((result.type = "success")) {
+        const fileName = result.name ?? "Unknown"; // Fallback to 'Unknown' if result.name is undefined
+        const fileSize = result.size ?? "Unknown"; // Fallback to 'Unknown' if result.size is undefined
+        Alert.alert("File Picked", `Name: ${fileName}\nSize: ${fileSize}`);
+        navigation.navigate("JobInterests"); // Navigate to JobInterestsScreen after successful upload
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Error", "An error occurred while picking the document.");
     }
-    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
